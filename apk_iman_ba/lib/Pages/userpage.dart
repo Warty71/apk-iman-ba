549f8f5dfe58aec7e1683bdc68336d43d49a5e8f
Korisnik <meris.lihic@gmail.com>
2023-06-26 13:55:09 +0200

    UserPage UI finished.

--- apk_iman_ba/lib/Pages/userpage.dart
+++ apk_iman_ba/lib/Pages/userpage.dart
@@ -1,5 +1,6 @@
 import 'package:apk_iman_ba/Pages/searchpage.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'favoritespage.dart';
 import 'homepage.dart';
@@ -90,8 +91,305 @@ class UserPage extends StatelessWidget {
           ),
         ),
       ),
-      body: const Center(
-        child: Text("Dodji sutra."),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(25.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Icon(
+                      Icons.person,
+                      color: Colors.black,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                      child: Text(
+                        "Moj Racun",
+                        style: GoogleFonts.poppins(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                          letterSpacing: 0.32,
+                          color: const Color(0xFF201E22),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.fromLTRB(25, 25, 25, 5),
+                width: double.infinity,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "E-mail",
+                      style: GoogleFonts.poppins(
+                        color: const Color(0xff201d22),
+                        fontSize: 14,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    Container(
+                      width: double.maxFinite,
+                      decoration: ShapeDecoration(
+                        shape: RoundedRectangleBorder(
+                          side: const BorderSide(
+                            width: 1.5,
+                            color: Color(0xFFEFF2F8),
+                          ),
+                          borderRadius: BorderRadius.circular(16),
+                        ),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(10, 15, 10, 15),
+                        child: Text(
+                          "meris.lihic@gmail.com",
+                          style: GoogleFonts.poppins(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w500,
+                            letterSpacing: 0.14,
+                            color: const Color(0xFF201D22),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              // PITANJA
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(
+                    25,
+                    25,
+                    25,
+                    5,
+                  ),
+                  child: Text(
+                    "PITANJA",
+                    style: GoogleFonts.poppins(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                      letterSpacing: 0.36,
+                      color: const Color(0xFF201E22),
+                    ),
+                  ),
+                ),
+              ),
+
+              // Moja Pitanja
+              Container(
+                margin: const EdgeInsets.fromLTRB(25, 5, 25, 10),
+                decoration: const BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(
+                      color: Color(0xCCEFF2F8),
+                      width: 3,
+                    ),
+                  ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          const Icon(Icons.person_outline, size: 24),
+                          Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 5.0),
+                            child: Text(
+                              "Moja Pitanja",
+                              style: GoogleFonts.poppins(
+                                fontSize: 14,
+                                letterSpacing: 0.28,
+                                color: const Color(0xFF201E22),
+                              ),
+                            ),
+                          ),
+                          const Text(
+                            "(12)",
+                          ),
+                        ],
+                      ),
+                      IconButton(
+                        iconSize: 20,
+                        icon: const Icon(
+                          Icons.arrow_forward_ios,
+                        ),
+                        onPressed: () {},
+                      )
+                    ],
+                  ),
+                ),
+              ),
+
+              // Zadnje otvarana pitanja
+              Container(
+                margin: const EdgeInsets.fromLTRB(25, 5, 25, 10),
+                decoration: const BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(
+                      color: Color(0xCCEFF2F8),
+                      width: 3,
+                    ),
+                  ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          const Icon(Icons.history, size: 24),
+                          Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 5.0),
+                            child: Text(
+                              "Zadnje otvarana pitanja",
+                              style: GoogleFonts.poppins(
+                                fontSize: 14,
+                                letterSpacing: 0.28,
+                                color: const Color(0xFF201E22),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      IconButton(
+                        iconSize: 20,
+                        icon: const Icon(
+                          Icons.arrow_forward_ios,
+                        ),
+                        onPressed: () {},
+                      )
+                    ],
+                  ),
+                ),
+              ),
+
+              // USLOVI KORISTENJA
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(
+                    25,
+                    25,
+                    25,
+                    5,
+                  ),
+                  child: Text(
+                    "USLOVI KORISTENJA",
+                    style: GoogleFonts.poppins(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                      letterSpacing: 0.36,
+                      color: const Color(0xFF201E22),
+                    ),
+                  ),
+                ),
+              ),
+
+              // FAQ
+              Container(
+                margin: const EdgeInsets.fromLTRB(25, 5, 25, 10),
+                decoration: const BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(
+                      color: Color(0xCCEFF2F8),
+                      width: 3,
+                    ),
+                  ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          const Icon(Icons.help_outline, size: 24),
+                          Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 5.0),
+                            child: Text(
+                              "FAQ",
+                              style: GoogleFonts.poppins(
+                                fontSize: 14,
+                                letterSpacing: 0.28,
+                                color: const Color(0xFF201E22),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      IconButton(
+                        iconSize: 20,
+                        icon: const Icon(
+                          Icons.arrow_forward_ios,
+                        ),
+                        onPressed: () {},
+                      )
+                    ],
+                  ),
+                ),
+              ),
+
+              // Uslovi koristenja
+              Container(
+                margin: const EdgeInsets.fromLTRB(25, 5, 25, 10),
+                decoration: const BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(
+                      color: Color(0xCCEFF2F8),
+                      width: 3,
+                    ),
+                  ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          const Icon(Icons.text_snippet_outlined, size: 24),
+                          Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 5.0),
+                            child: Text(
+                              "Uslovi koristenja",
+                              style: GoogleFonts.poppins(
+                                fontSize: 14,
+                                letterSpacing: 0.28,
+                                color: const Color(0xFF201E22),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      IconButton(
+                        iconSize: 20,
+                        icon: const Icon(
+                          Icons.arrow_forward_ios,
+                        ),
+                        onPressed: () {},
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
