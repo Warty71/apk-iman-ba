9b05d1c8feb718d81a15ce16a62eca0df1da9af6
Warty71 <meris.lihic@gmail.com>
2023-06-30 20:02:53 +0200

    UI fix. Authentication finished.

--- apk_iman_ba/lib/Pages/detailspage.dart
+++ apk_iman_ba/lib/Pages/detailspage.dart
@@ -81,116 +81,114 @@ class DetailsPage extends StatelessWidget {
         ),
       ),
       body: SafeArea(
-        child: Expanded(
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(10, 20, 10, 10),
-                  child: SizedBox(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        TextButton(
-                          onPressed: () {
-                            Navigator.pop(context);
-                          },
-                          style: TextButton.styleFrom(),
-                          child: Container(
-                            padding: const EdgeInsets.all(10),
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(16.0),
-                              color: const Color(0xffeff2f8),
-                            ),
-                            height: 48,
-                            child: Image.asset(
-                              "lib/Images/back.png",
-                              height: 4,
-                            ),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(10, 20, 10, 10),
+                child: SizedBox(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      TextButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        style: TextButton.styleFrom(),
+                        child: Container(
+                          padding: const EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(16.0),
+                            color: const Color(0xffeff2f8),
+                          ),
+                          height: 48,
+                          child: Image.asset(
+                            "lib/Images/back.png",
+                            height: 4,
                           ),
                         ),
-                        TextButton(
-                          onPressed: () {},
-                          style: TextButton.styleFrom(),
-                          child: Container(
-                            padding: const EdgeInsets.all(10),
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(16.0),
-                              color: const Color(0xffeff2f8),
-                            ),
-                            height: 48,
-                            child: const Icon(
-                              Icons.favorite_border,
-                              color: Colors.black,
-                              size: 28,
-                            ),
+                      ),
+                      TextButton(
+                        onPressed: () {},
+                        style: TextButton.styleFrom(),
+                        child: Container(
+                          padding: const EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(16.0),
+                            color: const Color(0xffeff2f8),
+                          ),
+                          height: 48,
+                          child: const Icon(
+                            Icons.favorite_border,
+                            color: Colors.black,
+                            size: 28,
                           ),
                         ),
-                      ],
-                    ),
-                  ),
-                ),
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 20.0, vertical: 10.0),
-                    child: Text(
-                      "Pitanje: ",
-                      style: GoogleFonts.poppins(
-                        fontSize: 14,
-                        fontWeight: FontWeight.w500,
-                        letterSpacing: 0.14,
-                        color: const Color(0xFF626164),
                       ),
+                    ],
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 20.0, vertical: 10.0),
+                  child: Text(
+                    "Pitanje: ",
+                    style: GoogleFonts.poppins(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w500,
+                      letterSpacing: 0.14,
+                      color: const Color(0xFF626164),
                     ),
                   ),
                 ),
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                    child: Text(
-                      title,
-                      style: GoogleFonts.poppins(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w500,
-                          color: const Color(0xFF201E22)),
-                    ),
-                  ),
-                ),
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Padding(
-                    padding: const EdgeInsets.fromLTRB(20, 20, 20, 5),
-                    child: Text(
-                      "Odgovor: ",
-                      style: GoogleFonts.poppins(
-                        fontSize: 14,
+              ),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  child: Text(
+                    title,
+                    style: GoogleFonts.poppins(
+                        fontSize: 20,
                         fontWeight: FontWeight.w500,
-                        letterSpacing: 0.14,
-                        color: const Color(0xFF626164),
-                      ),
+                        color: const Color(0xFF201E22)),
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(20, 20, 20, 5),
+                  child: Text(
+                    "Odgovor: ",
+                    style: GoogleFonts.poppins(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w500,
+                      letterSpacing: 0.14,
+                      color: const Color(0xFF626164),
                     ),
                   ),
                 ),
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Padding(
-                    padding: const EdgeInsets.fromLTRB(20, 5, 20, 5),
-                    child: Text(
-                      answer,
-                      style: GoogleFonts.poppins(
-                        fontSize: 14,
-                        fontWeight: FontWeight.w500,
-                        letterSpacing: 0.28,
-                        color: const Color(0xFF626164),
-                      ),
+              ),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(20, 5, 20, 5),
+                  child: Text(
+                    answer,
+                    style: GoogleFonts.poppins(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w500,
+                      letterSpacing: 0.28,
+                      color: const Color(0xFF626164),
                     ),
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
--- apk_iman_ba/lib/Pages/loginpage.dart
+++ apk_iman_ba/lib/Pages/loginpage.dart
@@ -10,6 +10,44 @@ class LoginPage extends StatelessWidget {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
 
+  void wrongEmailMessage(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text("Pogresan email"),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text("Zatvori"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void wrongPasswordMessage(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text("Pogresna sifra"),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text("Zatvori"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   Future<void> signUserIn(BuildContext context) async {
     showDialog(
       context: context,
@@ -20,7 +58,7 @@ class LoginPage extends StatelessWidget {
             width: 80.0,
             height: 80.0,
             decoration: BoxDecoration(
-              color: Colors.white,
+              color: Colors.transparent,
               borderRadius: BorderRadius.circular(8.0),
             ),
             child: const CircularProgressIndicator(),
@@ -34,14 +72,16 @@ class LoginPage extends StatelessWidget {
         email: emailController.text,
         password: passwordController.text,
       );
-      // ignore: avoid_print
-      print("Signed in successfully.");
-    } catch (e) {
-      // ignore: avoid_print
-      print("Sign-in failed: $e");
+      // ignore: use_build_context_synchronously
+      Navigator.pop(context);
+    } on FirebaseAuthException catch (e) {
+      Navigator.pop(context);
+      if (e.code == 'user-not-found') {
+        wrongEmailMessage(context);
+      } else if (e.code == 'wrong-password') {
+        wrongPasswordMessage(context);
+      }
     }
-    // ignore: use_build_context_synchronously
-    Navigator.pop(context);
   }
 
   @override
@@ -55,198 +95,200 @@ class LoginPage extends StatelessWidget {
         resizeToAvoidBottomInset: false,
         body: SafeArea(
           child: Center(
-            child: Column(
-              children: [
-                // Iman Logo
-                Padding(
-                  padding: const EdgeInsets.only(top: 20.0),
-                  child: SizedBox(
-                    height: 100,
-                    width: 100,
-                    child: Image.asset("lib/Images/Logo.jpg"),
-                  ),
-                ),
-
-                // Dobrodosli na IMAN
-                Padding(
-                  padding: const EdgeInsets.only(top: 10.0),
-                  child: Text(
-                    "Dobrodosli na IMAN",
-                    style: GoogleFonts.poppins(
-                      color: const Color(0xff190c3f),
-                      fontSize: 28,
-                      fontWeight: FontWeight.w500,
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  // Iman Logo
+                  Padding(
+                    padding: const EdgeInsets.only(top: 25.0),
+                    child: SizedBox(
+                      height: 100,
+                      width: 100,
+                      child: Image.asset("lib/Images/Logo.jpg"),
                     ),
                   ),
-                ),
 
-                // Prijavite se na svoj IMAN racun ili kreirajte novi
-                Padding(
-                  padding: const EdgeInsets.only(top: 5.0),
-                  child: Text(
-                    "Prijavite se na svoj IMAN racun\n ili kreirajte novi",
-                    textAlign: TextAlign.center,
-                    style: GoogleFonts.poppins(
-                      color: const Color(0xff626164),
-                      letterSpacing: 0.32,
-                      fontSize: 16,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                ),
-
-                // E-mail text i field za unos e-maila
-                Container(
-                  margin: const EdgeInsets.fromLTRB(25, 25, 25, 5),
-                  width: double.infinity,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "E-mail",
-                        style: GoogleFonts.poppins(
-                          color: const Color(0xff201d22),
-                          fontSize: 14,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                      CustomTextField(
-                        controller: emailController,
-                        hintText: 'Vas e-mail',
-                        obscureText: false,
-                      ),
-                    ],
-                  ),
-                ),
-
-                // Sifra i textfield za unos sifre
-                Container(
-                  margin: const EdgeInsets.fromLTRB(25, 5, 25, 10),
-                  width: double.infinity,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "Sifra",
-                        style: GoogleFonts.poppins(
-                          color: const Color(0xff201d22),
-                          fontSize: 14,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                      CustomTextField(
-                        hintText: "**********",
-                        controller: passwordController,
-                        obscureText: true,
-                      )
-                    ],
-                  ),
-                ),
-
-                // Zaboravili ste sifru?
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: Padding(
-                    padding: const EdgeInsets.only(right: 25.0),
-                    child: GestureDetector(
-                      onTap: () {},
-                      child: Text(
-                        "Zaboravili ste sifru?",
-                        style: GoogleFonts.poppins(
-                          fontSize: 14,
-                          fontWeight: FontWeight.w500,
-                          color: const Color(0xff5449d2),
-                        ),
+                  // Dobrodosli na IMAN
+                  Padding(
+                    padding: const EdgeInsets.only(top: 10.0),
+                    child: Text(
+                      "Dobrodosli na IMAN",
+                      style: GoogleFonts.poppins(
+                        color: const Color(0xff190c3f),
+                        fontSize: 28,
+                        fontWeight: FontWeight.w500,
                       ),
                     ),
                   ),
-                ),
 
-                // "Prijavi se - button"
-                GestureDetector(
-                  onTap: () {
-                    signUserIn(context);
-                  },
-                  child: Container(
-                    margin: const EdgeInsets.fromLTRB(0, 10.0, 0, 10.0),
+                  // Prijavite se na svoj IMAN racun ili kreirajte novi
+                  Padding(
+                    padding: const EdgeInsets.only(top: 5.0),
+                    child: Text(
+                      "Prijavite se na svoj IMAN racun\n ili kreirajte novi",
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.poppins(
+                        color: const Color(0xff626164),
+                        letterSpacing: 0.32,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+
+                  // E-mail text i field za unos e-maila
+                  Container(
+                    margin: const EdgeInsets.fromLTRB(25, 25, 25, 5),
+                    width: double.infinity,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "E-mail",
+                          style: GoogleFonts.poppins(
+                            color: const Color(0xff201d22),
+                            fontSize: 14,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        CustomTextField(
+                          controller: emailController,
+                          hintText: 'Vas e-mail',
+                          obscureText: false,
+                        ),
+                      ],
+                    ),
+                  ),
+
+                  // Sifra i textfield za unos sifre
+                  Container(
+                    margin: const EdgeInsets.fromLTRB(25, 5, 25, 10),
+                    width: double.infinity,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Sifra",
+                          style: GoogleFonts.poppins(
+                            color: const Color(0xff201d22),
+                            fontSize: 14,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        CustomTextField(
+                          hintText: "**********",
+                          controller: passwordController,
+                          obscureText: true,
+                        )
+                      ],
+                    ),
+                  ),
+
+                  // Zaboravili ste sifru?
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: Padding(
+                      padding: const EdgeInsets.only(right: 25.0),
+                      child: GestureDetector(
+                        onTap: () {},
+                        child: Text(
+                          "Zaboravili ste sifru?",
+                          style: GoogleFonts.poppins(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w500,
+                            color: const Color(0xff5449d2),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  // "Prijavi se - button"
+                  GestureDetector(
+                    onTap: () {
+                      signUserIn(context);
+                    },
+                    child: Container(
+                      margin: const EdgeInsets.fromLTRB(0, 10.0, 0, 5),
+                      width: 325,
+                      height: 64,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(24),
+                        gradient: const RadialGradient(
+                          center: Alignment(-1.538, -3.615),
+                          radius: 4,
+                          colors: <Color>[Color(0xff5959e4), Color(0xff140632)],
+                          stops: <double>[0, 0.99],
+                        ),
+                      ),
+                      child: const Center(
+                        child: Text(
+                          "Prijavi se",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                            fontFamily: "Poppins",
+                            fontWeight: FontWeight.w500,
+                            letterSpacing: 0.36,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  // Prijavite se preko Google racuna
+                  Padding(
+                    padding: const EdgeInsets.only(top: 15.0),
+                    child: Text(
+                      "Prijavite se preko Google racuna",
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.poppins(
+                        color: const Color(0xff626164),
+                        letterSpacing: 0.32,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+
+                  // Google Log In
+                  Container(
+                    margin: const EdgeInsets.fromLTRB(0, 10.0, 0, 0),
                     width: 325,
                     height: 64,
                     decoration: BoxDecoration(
+                      border: Border.all(
+                        color: const Color(0xff626164),
+                      ),
                       borderRadius: BorderRadius.circular(24),
-                      gradient: const RadialGradient(
-                        center: Alignment(-1.538, -3.615),
-                        radius: 4,
-                        colors: <Color>[Color(0xff5959e4), Color(0xff140632)],
-                        stops: <double>[0, 0.99],
-                      ),
                     ),
-                    child: const Center(
-                      child: Text(
-                        "Prijavi se",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 18,
-                          fontFamily: "Poppins",
-                          fontWeight: FontWeight.w500,
-                          letterSpacing: 0.36,
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.fromLTRB(0, 0, 10.0, 0),
+                          width: 24,
+                          height: 24,
+                          child: Image.asset("lib/Images/google.png"),
                         ),
-                      ),
-                    ),
-                  ),
-                ),
-
-                // Prijavite se preko Google racuna
-                Padding(
-                  padding: const EdgeInsets.only(top: 15.0),
-                  child: Text(
-                    "Prijavite se preko Google racuna",
-                    textAlign: TextAlign.center,
-                    style: GoogleFonts.poppins(
-                      color: const Color(0xff626164),
-                      letterSpacing: 0.32,
-                      fontSize: 16,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                ),
-
-                // Google Log In
-                Container(
-                  margin: const EdgeInsets.fromLTRB(0, 10.0, 0, 0),
-                  width: 325,
-                  height: 64,
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: const Color(0xff626164),
-                    ),
-                    borderRadius: BorderRadius.circular(24),
-                  ),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Container(
-                        margin: const EdgeInsets.fromLTRB(0, 0, 10.0, 0),
-                        width: 24,
-                        height: 24,
-                        child: Image.asset("lib/Images/google.png"),
-                      ),
-                      const Text(
-                        "Google Login",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          color: Color(0xff24195b),
-                          fontSize: 18,
-                          fontFamily: "Poppins",
-                          fontWeight: FontWeight.w500,
-                          letterSpacing: 0.36,
+                        const Text(
+                          "Google Login",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Color(0xff24195b),
+                            fontSize: 18,
+                            fontFamily: "Poppins",
+                            fontWeight: FontWeight.w500,
+                            letterSpacing: 0.36,
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
