a011446731f4fb3addb658651eaacd7ca5be791d
Warty71 <meris.lihic@gmail.com>
2023-07-06 21:00:07 +0200

    Questions edited

--- apk_iman_ba/lib/models/question_model.dart
+++ apk_iman_ba/lib/models/question_model.dart
@@ -7,5 +7,18 @@ class Question {
   String? datum;
 
   Question(
-      {this.pitanje, this.odgovor, this.pitao, this.odgovorio, this.datum});
+      {this.key,
+      this.pitanje,
+      this.odgovor,
+      this.pitao,
+      this.odgovorio,
+      this.datum});
+
+  Question.fromJson(Map<dynamic, dynamic> json) {
+    pitanje = json["pitanje"];
+    odgovor = json["odgovor"];
+    pitao = json["pitao"];
+    odgovorio = json["odgovorio"];
+    datum = json["datum"];
+  }
 }
