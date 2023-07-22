8543cfa3853cb804b655b633081be67e92747b28
Warty71 <meris.lihic@gmail.com>
2023-07-22 12:41:48 +0200

    Disabled the native back button.

--- apk_iman_ba/lib/Components/custom_bnb.dart
+++ apk_iman_ba/lib/Components/custom_bnb.dart
@@ -24,53 +24,58 @@ class _CustomBNBState extends State<CustomBNB> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: _pages[_currentIndex],
-      bottomNavigationBar: Padding(
-        padding: const EdgeInsets.fromLTRB(10, 0, 10, 10),
-        child: Container(
-          padding: const EdgeInsets.all(4.0),
-          margin: const EdgeInsets.symmetric(horizontal: 24),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(20),
-            gradient: const RadialGradient(
-              center: Alignment(-1.538, -3.615),
-              radius: 4,
-              colors: <Color>[Color(0xff5959e4), Color(0xff140632)],
-              stops: <double>[0, 0.99],
+    return WillPopScope(
+      onWillPop: () async {
+        return false;
+      },
+      child: Scaffold(
+        body: _pages[_currentIndex],
+        bottomNavigationBar: Padding(
+          padding: const EdgeInsets.fromLTRB(10, 0, 10, 10),
+          child: Container(
+            padding: const EdgeInsets.all(4.0),
+            margin: const EdgeInsets.symmetric(horizontal: 24),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              gradient: const RadialGradient(
+                center: Alignment(-1.538, -3.615),
+                radius: 4,
+                colors: <Color>[Color(0xff5959e4), Color(0xff140632)],
+                stops: <double>[0, 0.99],
+              ),
             ),
-          ),
-          child: BottomNavigationBar(
-            currentIndex: _currentIndex,
-            onTap: (index) {
-              setState(() {
-                _currentIndex = index;
-              });
-            },
-            backgroundColor: Colors.transparent,
-            selectedItemColor: Colors.white,
-            unselectedItemColor: Colors.white54,
-            type: BottomNavigationBarType.fixed,
-            showSelectedLabels: false, // Hide labels for selected items
-            showUnselectedLabels: false, // Hide labels for unselected items
-            items: const [
-              BottomNavigationBarItem(
-                icon: Icon(Icons.home_outlined),
-                label: "Home",
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.search),
-                label: "Search",
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.favorite_border),
-                label: "Favorites",
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.person_4_outlined),
-                label: "User",
-              ),
-            ],
+            child: BottomNavigationBar(
+              currentIndex: _currentIndex,
+              onTap: (index) {
+                setState(() {
+                  _currentIndex = index;
+                });
+              },
+              backgroundColor: Colors.transparent,
+              selectedItemColor: Colors.white,
+              unselectedItemColor: Colors.white54,
+              type: BottomNavigationBarType.fixed,
+              showSelectedLabels: false, // Hide labels for selected items
+              showUnselectedLabels: false, // Hide labels for unselected items
+              items: const [
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.home_outlined),
+                  label: "Home",
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.search),
+                  label: "Search",
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.favorite_border),
+                  label: "Favorites",
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.person_4_outlined),
+                  label: "User",
+                ),
+              ],
+            ),
           ),
         ),
       ),
