9a8be81dd47baa6a2275008ab30bce0a85506f5c
mac <meris.lihic@gmail.com>
2023-09-23 11:39:36 +0200

    AlertService Snackbars added.

--- apk_iman_ba/lib/Services/alert_service.dart
+++ apk_iman_ba/lib/Services/alert_service.dart
@@ -1,96 +1,53 @@
-import 'package:apk_iman_ba/Pages/Extra/auth_page.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AlertService {
-  static void _showPlatformSpecificAlertDialog(
+  static void showSnackbar(
     BuildContext context, {
-    required String title,
-    required String content,
+    required String message,
   }) {
-    if (Theme.of(context).platform == TargetPlatform.android) {
-      showDialog(
-        context: context,
-        builder: (context) {
-          return AlertDialog(
-            title: Text(title),
-            content: Text(content),
-            actions: [
-              TextButton(
-                onPressed: () {
-                  Navigator.pushAndRemoveUntil(
-                    context,
-                    MaterialPageRoute(builder: (_) => const AuthPage()),
-                    (route) => false,
-                  );
-                },
-                child: const Text("OK"),
-              ),
-            ],
-          );
-        },
-      );
-    } else if (Theme.of(context).platform == TargetPlatform.iOS) {
-      showCupertinoDialog(
-        context: context,
-        builder: (context) {
-          return CupertinoAlertDialog(
-            title: Text(title),
-            content: Text(content),
-            actions: [
-              CupertinoDialogAction(
-                onPressed: () {
-                  Navigator.pushAndRemoveUntil(
-                    context,
-                    MaterialPageRoute(builder: (_) => const AuthPage()),
-                    (route) => false,
-                  );
-                },
-                child: const Text("OK"),
-              ),
-            ],
-          );
-        },
-      );
-    }
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(
+          message,
+          style: GoogleFonts.poppins(
+            fontSize: 14,
+          ),
+        ),
+        duration: const Duration(seconds: 3),
+      ),
+    );
   }
 
 // On Sign-In, if the e-mail is incorrect.
   static void showWrongEmailMessage(BuildContext context) {
-    _showPlatformSpecificAlertDialog(
+    showSnackbar(
       context,
-      title: "Pogresan e-mail",
-      content:
-          "E-mail koji ste unijeli nije registrovan.", // Customize the content here
+      message: "E-mail koji ste unijeli nije registrovan.",
     );
   }
 
 // On Sign-In, if the password is incorrect.
   static void showWrongPasswordMessage(BuildContext context) {
-    _showPlatformSpecificAlertDialog(
+    showSnackbar(
       context,
-      title: "Pogresna šifra",
-      content:
-          "Šifra koju ste unijeli nije tačna.", // Customize the content here
+      message: "Šifra koju ste unijeli nije tačna.",
     );
   }
 
   static void showEmailVerifMessageSuccess(BuildContext context) {
-    _showPlatformSpecificAlertDialog(
+    showSnackbar(
       context,
-      title: "Registracija uspješna",
-      content: "Na Vašu email adresu je poslan verifikacijski email. "
-          "Molimo Vas da verifikujete svoj email prije prijave.", // Customize the content here
+      message: "Na Vašu email adresu je poslan verifikacijski email. "
+          "Molimo Vas da verifikujete svoj email prije prijave.",
     );
   }
 
   // Feature coming soon alert window.
   static void showComingSoon(BuildContext context) {
-    _showPlatformSpecificAlertDialog(
+    showSnackbar(
       context,
-      title: "Uskoro!",
-      content:
-          "InšAllah, ubrzo dolazi ova funkcija.", // Customize the content here
+      message: "InšAllah, ubrzo dolazi ova funkcija.",
     );
   }
 }
